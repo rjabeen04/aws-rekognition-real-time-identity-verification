8814e22cbc2303a3813e65466c18a07014fa37cf
rjabeen04 <rjabeen122@gmail.com>
2026-07-09 12:41:26 -0500

    feat(terraform): configure Lambda IAM role and least-privilege policy

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -26,3 +26,68 @@ resource "aws_iam_role" "lambda_execution_role" {
 
 
 
+resource "aws_iam_policy" "lambda_policy" {
+  name        = "secureguard-lambda-policy"
+  description = "Least privilege policy for SecureGuard Lambda"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+
+      {
+        Sid    = "S3ReadAccess"
+        Effect = "Allow"
+
+        Action = [
+          "s3:GetObject"
+        ]
+
+        Resource = [
+          "${aws_s3_bucket.raw_captures.arn}/*"
+        ]
+      },
+
+      {
+        Sid    = "RekognitionAccess"
+        Effect = "Allow"
+
+        Action = [
+          "rekognition:SearchFacesByImage",
+          "rekognition:DetectFaces",
+          "rekognition:DetectLabels"
+        ]
+
+        Resource = "*"
+      },
+
+      {
+        Sid    = "CloudWatchLogs"
+        Effect = "Allow"
+
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+
+        Resource = "*"
+      }
+    ]
+  })
+
+  tags = {
+    Name    = "SecureGuard Lambda Policy"
+    Project = "SecureGuard"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+  role       = aws_iam_role.lambda_execution_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
+
+
+
+
+
